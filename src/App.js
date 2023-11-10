9ed955d946dafad1f15797fe2f843218509097c7
s1f102100793 <s1f102100793@iniad.org>
2023-11-11 00:40:03 +0900

    make: simple counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const add = () => {
+    setCount(prevCount => Math.min(100, prevCount + 1));
+  };
+
+  const sub = () => {
+    setCount(prevCount => Math.max(0, prevCount - 1));
+  };
+
+  const mul = () => {
+    setCount(prevCount => Math.min(100, prevCount * 2));
+  };
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='buttoncontainer'>
+      <button id='add' onClick={add} className='button'>+1</button>
+      <button id='sub' onClick={sub} className='button'>-1</button>
+      <button id='mul' onClick={mul} className='button'>*2</button>
+      </div>
+      <div id='count' className='counter'>{count}</div>
+      {/* <button id='reset' onClick={() => setCount(0)} className='button'>Reset</button> */}
     </div>
   );
 }
